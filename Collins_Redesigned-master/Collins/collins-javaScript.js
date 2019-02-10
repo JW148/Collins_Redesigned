9735df2bf12ae7d0defe5c19fad1810026d7e81e
Wiggaazz <47108195+Wiggaazz@users.noreply.github.com>
2019-02-10 20:22:49 +0000

    Add files via upload

--- Collins_Redesigned-master/Collins/collins-javaScript.js
+++ Collins_Redesigned-master/Collins/collins-javaScript.js
@@ -20,23 +20,46 @@ function FilteringBooks() {
     }
 }
 
-var span1 = document.getElementsByClassName("close")[0];
-var modal1 = document.getElementById('myModal1');
-var img1 = document.getElementById('myImg1');
 
-img1.onclick = function() {
-    modal1.style.display = "block";
-}
+var modalImgs = [...document.querySelectorAll(".bookCover")];
+modalImgs.forEach(function(img){
+  img.onclick = function() {
+    var modal = img.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
+});
 
-span1.onclick = function() {
-    modal1.style.display = "none";
-}
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
+    modal.style.display = "none";
+  }
+});
 
 window.onclick = function(event) {
-    if(event.target == modal1) {
-        modal1.style.display = "none";
-    }
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
 }
+
+//var span1 = document.getElementsByClassName("close")[0];
+//var modal1 = document.getElementById('myModal1');
+//var img1 = document.getElementById('myImg1');
+//
+//img1.onclick = function() {
+//    modal1.style.display = "block";
+//}
+//
+//span1.onclick = function() {
+//    modal1.style.display = "none";
+//}
+//
+//window.onclick = function(event) {
+//    if(event.target == modal1) {
+//        modal1.style.display = "none";
+//    }
+//}
 
 //var span2 = document.getElementsByClassName("close")[0];
 //var modal2 = document.getElementById('myModal2');
