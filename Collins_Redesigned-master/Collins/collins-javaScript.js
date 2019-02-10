41d9fce6e016c818810fe34c7ea540a4dc340518
Wiggaazz <47108195+Wiggaazz@users.noreply.github.com>
2019-02-10 15:25:30 +0000

    Add files via upload

--- Collins_Redesigned-master/Collins/collins-javaScript.js
+++ Collins_Redesigned-master/Collins/collins-javaScript.js
@@ -1,20 +1,42 @@
+
 function FilteringBooks() {
-    var UserInput, Filter, ul, list, o, counter, ValueOfText;
+    var UserInput, Filter, ul, li, o, counter, ValueOfText;
     
     UserInput = document.getElementById('myBooks');
     Filter = UserInput.value.toUpperCase();
     ul = document.getElementById("myUL");
-    list = ul.getElementsByTagName('list');
+    li = ul.getElementsByTagName("li");
     
-    for (counter = 0; counter < list.length; counter++) {
+    for (counter = 0; counter < li.length; counter++) {
         
-        o = list[counter].getElementsByTagName("a")[0];
+        o = li[counter].getElementsByTagName("a")[0];
         ValueOfText = o.textContent || o.innerText;
         
         if (ValueOfText.toUpperCase().indexOf(Filter) > -1) {
-            list[counter].style.display = "";
+            li[counter].style.display = "";
         } else {
-            list[counter].style.display = "none";
+            li[counter].style.display = "none";
         }
+    }
+}
+
+var modal = document.getElementById('myModal');
+
+
+var img = document.getElementById('myImg');
+
+var span = document.getElementsByClassName("close")[0];
+
+img.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
     }
 }
