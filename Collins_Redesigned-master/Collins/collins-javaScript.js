e79b2b44fb2c3b9810a2e6a1f3ebdd884377afd0
eniolubifaleye <47322206+eniolubifaleye@users.noreply.github.com>
2019-02-11 14:08:08 +0000

    Add files via upload
    tidy

--- Collins_Redesigned-master/Collins/collins-javaScript.js
+++ Collins_Redesigned-master/Collins/collins-javaScript.js
@@ -20,18 +20,9 @@ function FilteringBooks() {
     }
 }
 
-//gets all the elements with the class bookCover and assigns it to the variable modalImgs
-var modalImgs = [...document.querySelectorAll(".bookCover")];
-modalImgs.forEach(function(img){    //for each modalImg...
-  img.onclick = function() {        //when it's clicked
-    var modal = img.getAttribute('data-modal');     //get the value of the data attribute. this is assigned to the correct book modal. sets it to the variable modal
-    document.getElementById(modal).style.display = "block";     //enables the correct modal
-  }
-});
 
-//does the same as above 
-var modalList = [...document.querySelectorAll(".bookList")];
-modalList.forEach(function(img){
+var modalImgs = [...document.querySelectorAll(".bookCover")];
+modalImgs.forEach(function(img){
   img.onclick = function() {
     var modal = img.getAttribute('data-modal');
     document.getElementById(modal).style.display = "block";
@@ -52,57 +43,3 @@ window.onclick = function(event) {
   }
 }
 
-//var span1 = document.getElementsByClassName("close")[0];
-//var modal1 = document.getElementById('myModal1');
-//var img1 = document.getElementById('myImg1');
-//
-//img1.onclick = function() {
-//    modal1.style.display = "block";
-//}
-//
-//span1.onclick = function() {
-//    modal1.style.display = "none";
-//}
-//
-//window.onclick = function(event) {
-//    if(event.target == modal1) {
-//        modal1.style.display = "none";
-//    }
-//}
-
-//var span2 = document.getElementsByClassName("close")[0];
-//var modal2 = document.getElementById('myModal2');
-//var img2 = document.getElementById('myImg2');
-//
-//img2.onclick = function() {
-//    modal2.style.display = "block";
-//}
-//
-//span2.onclick = function() {
-//    modal2.style.display = "none";
-//}
-//
-//window.onclick = function(event) {
-//    if(event.target == modal2) {
-//        modal2.style.display = "none";
-//    }
-//}
-//
-//var span3 = document.getElementsByClassName("close")[0];
-//var modal3 = document.getElementById('myModal3');
-//var img3 = document.getElementById('myImg3');
-//
-//img3.onclick = function() {
-//    modal3.style.display = "block";
-//}
-//
-//span3.onclick = function() {
-//    modal3.style.display = "none";
-//}
-//
-//window.onclick = function(event) {
-//    if(event.target == modal3) {
-//        modal3.style.display = "none";
-//    }
-//}
-//
